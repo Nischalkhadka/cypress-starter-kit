b17e5e7fd72ef6778df8355db84d0541be588706
Nischal Khadka <nischalkhadka@lftechnology.com>
2020-06-11 14:55:44 +0545

    test-002:Update test and Readme

--- cypress/integration/tests/sample_test.js
+++ cypress/integration/tests/sample_test.js
@@ -1,16 +1,11 @@
-describe ('Verify that the Author is Nischal Khadka', () =>{
+describe ('Search for Leapfrog technology ', () =>{
     
-    it("should visit the blog", () => {
-        cy.visit("/blog/device-fragmentation-during-mobile-testing/")
+    it("should visit Google Homepage", () => {
+        cy.visit("")
     })
-
-    it("should scroll to the Author section", () =>{
-        cy.get('article > :nth-child(28)').scrollIntoView()
-        .should('be.visible')
-    })
-
-    it("should verify the author", () =>{
-        cy.get('article > :nth-child(29)').contains('Nischal Khadka')
+    
+    it("should search for Leapfrog", () => {
+        cy.get('.gLFyf').type('Leapfrog technology').type('{enter}')
     })
 
 })
