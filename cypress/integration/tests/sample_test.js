ec4413601b21735b8c5aaf62c0085ef69f11e1aa
Nischal Khadka <nischalkhadka@lftechnology.com>
2020-06-11 14:01:23 +0545

    test-002:Add test to verify the author

---
+++ cypress/integration/tests/sample_test.js
@@ -0,0 +1,16 @@
+describe ('Verify that the Author is Nischal Khadka', () =>{
+    it("should visit the blog", () => {
+        cy.visit("/blog/device-fragmentation-during-mobile-testing/")
+    })
+
+    it("should scroll to the Author section", () =>{
+        cy.get('article > :nth-child(28)').scrollIntoView()
+        .should('be.visible')
+    })
+
+    it("should verify the author", () =>{
+        cy.get('article > :nth-child(29)').contains('Nischal Khadka')
+    })
+
+
+})
